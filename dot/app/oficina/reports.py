9f1ffccdc9cc49adc03645d809447977084cd696
Rafael Gustavo Alves <castelhano.rafael@gmail.com>
2022-01-18 21:30:25 -0400

    Ajustes frota_dashboard

--- dot/app/oficina/reports.py
+++ dot/app/oficina/reports.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
 from .models import Frota, Componente
@@ -15,7 +15,17 @@ def frota_dashboard(request):
         data_simulada = datetime.strptime(request.GET.get('data_simulada', None), '%Y-%m-%d').date()
     else:
         data_simulada = date.today()
-    frota = Frota.objects.all().exclude(status__in=['V', 'I'])
+    frota = Frota.objects.all().exclude(status__in=['V', 'I']).order_by('prefixo')
+    
+    if request.GET.get('empresa', None):
+        if request.user.is_superuser or request.user.profile.empresas.filter(id=request.GET.get('empresa', None)):
+            frota = frota.filter(empresa__id=request.GET.get('empresa', None))
+        else:
+            messages.error(request,'Empresa <b>não habilitada</b> para o seu usuário')
+            return redirect('oficina_frota_dashboard')
+    else:
+        if not request.user.is_superuser:
+            frota = frota.filter(empresa__id__in=request.user.profile.empresas.all())
     
     componentes = Componente.objects.filter(frota_componentes__in=frota).annotate(qtde=Count('nome')).order_by('nome')
     componentes_metrics = {}
@@ -25,9 +35,9 @@ def frota_dashboard(request):
     if not request.GET.get('resumo_por', None) or request.GET.get('resumo_por', None) == 'aniversario':
         frota_idade = frota.exclude(aniversario=None).annotate(ano=TruncYear('aniversario')).values('ano').annotate(total=Count('ano'))
     elif request.GET.get('resumo_por', None) == 'ano_fabricacao':
-        frota_idade = frota.annotate(ano=F('ano_fabricacao')).values('ano').annotate(total=Count('ano'))
+        frota_idade = frota.exclude(ano_fabricacao=None).annotate(ano=F('ano_fabricacao')).values('ano').annotate(total=Count('ano'))
     elif request.GET.get('resumo_por', None) == 'ano_modelo':
-        frota_idade = frota.annotate(ano=F('ano_modelo')).values('ano').annotate(total=Count('ano'))
+        frota_idade = frota.exclude(ano_modelo=None).annotate(ano=F('ano_modelo')).values('ano').annotate(total=Count('ano'))
     else:
         messages.error(request,'Opção de resumo <b>inválida</b>')
     
@@ -42,26 +52,32 @@ def frota_dashboard(request):
     soma_idade_frota = 0
     qtde_idade_frota = 0
     idade_frota_ignorada = []
-    marcas = {}
+    marcas = dict()
+    marcas_sum = dict()
     for f in frota: # Percore a frota para calculo da idade media e resumo das marcas e modelos
         if f.modelo: # Verifica se o modelo do carro foi preenchido
             if f.modelo.marca.nome in marcas: # Verifica se ja existe entrada para marca no dicionario de marcas
-                marcas[f.modelo.marca.nome][f.modelo.nome] += 1 if f.modelo.nome in marcas[f.modelo.marca.nome] else 1
+                marcas_sum[f.modelo.marca.nome] += 1
+                if f.modelo.nome in marcas[f.modelo.marca.nome]:
+                    marcas[f.modelo.marca.nome][f.modelo.nome] += 1
+                else:
+                    marcas[f.modelo.marca.nome][f.modelo.nome] = 1                    
             else:
+                marcas_sum[f.modelo.marca.nome] = 1
                 marcas[f.modelo.marca.nome] = {f.modelo.nome:1}
         # *********************************************** 
         if request.GET.get('resumo_por', None) == 'ano_fabricacao' and f.ano_fabricacao:
-            soma_idade_frota += f.idade_ano_fabricacao()
+            soma_idade_frota += f.idade_ano_fabricacao(data_simulada)
             qtde_idade_frota += 1
         elif request.GET.get('resumo_por', None) == 'ano_modelo' and f.ano_modelo:
-            soma_idade_frota += f.idade_ano_modelo()
+            soma_idade_frota += f.idade_ano_modelo(data_simulada)
             qtde_idade_frota += 1
         elif (not request.GET.get('resumo_por', None) or request.GET.get('resumo_por', None) == 'aniversario') and f.aniversario:
             soma_idade_frota += f.idade_aniversario(data_simulada)
             qtde_idade_frota += 1
         else:
             idade_frota_ignorada.append(f.prefixo)
-    print(marcas)
+    
     for row in frota_idade: # Percorre frota para preencher frota por ano (para grafico)
         frota_idade_metrics['categorias'].append(row['ano'].year if request.GET.get('resumo_por', None) in ['aniversario', None] else row['ano'])
         frota_idade_metrics['dados'].append(float(row['total']))
@@ -77,6 +93,7 @@ def frota_dashboard(request):
         'idade_frota_ignorada':idade_frota_ignorada,
         'idade_media':soma_idade_frota / qtde_idade_frota if qtde_idade_frota > 0 else '---',
         'marcas':marcas,
+        'marcas_sum':marcas_sum,
         'componentes':componentes_metrics,
     }
     metrics['frota_parada_percentual'] = round(((metrics['frota_fora_operacao'] + metrics['frota_oficina']) / metrics['frota_ativa']) * 100) if metrics['frota_ativa'] > 0 else '---'
