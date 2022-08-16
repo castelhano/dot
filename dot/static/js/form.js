7a33753069e9e6f8c4d1c5293d14a665acfa8bb0
Rafael Gustavo Alves <castelhano.rafael@gmail.com>
2022-08-16 19:30:07 -0400

    jsForm alpha 3

--- dot/static/js/form.js
+++ dot/static/js/form.js
@@ -1,76 +1,44 @@
 let obj = [
     {
-        "action":"foo_fei",
-        "method":"post",
-        "autocomplete":"off",
-        "fields": {
-            "csrfmiddlewaretoken":{
-                "type":"hidden",
-                "name":"csrfmiddlewaretoken",
-                "id":"id_csrfmiddlewaretoken",
-                "value":""
-            },
-            "Age":{
-                "name": "age",
-                "id": "id_age",
-                "value": "22",
-                "class": "form-control",
-                "type": "number",
-                "min": "1",
-                "max": "35",
-                "data-type":"primary-key"
-            },
-            "Owner":{
-                "name": "owner",
-                "id": "id_owner",
-                "value": "Rafael",
-                "class": "form-control",
-                "type": "text",
-                "autofocus": "autofocus",
-                "onclick":"console.log('fooo')"
-            }
-        }
+        "name":"nome",
+        "type":"text",
+        "group":"Cadastro",
+        "value":"Rafael Gustavo Faria Alves"
+    },
+    {
+        "name":"email",
+        "type":"email",
+        "group":"Cadastro",
+        "value":"castelhano.rafael@gmail.com"
+    },
+    {
+        "name":"fone",
+        "type":"text",
+        "group":"Contato",
+        "value":"(65) 9 8145 0188"
     }
 ]
 class jsForm{
     constructor(options){
         this.data = options?.data || []; // Json com dados do form
         this.container = options?.container || document.body; // parentNode do form, caso nao informado append no document.body
-
+        this.formClassList = options?.formClassList || 'table border';
+        this.keyClassList = options?.keyClassList || 'text-capitalize';
+        this.valueClassList = options?.valueClassList || 'bg-light border';
+        
         this.createForm();
     }
     createForm(){
-        let form = document.createElement('form');
-        for(let item in this.data[0]){if(item != 'fields'){form.setAttribute(item, this.data[0][item]);}} // Monta composicao basica do form
-
-        for(let field in this.data[0].fields){
-            let f = document.createElement('input');
-            for(let attr in this.data[0].fields[field]){
-                f.setAttribute(attr, this.data[0].fields[field][attr]);
-            }
-            if(f.type != 'hidden'){form.appendChild(this.buildFormRow(f));}
-            else{form.appendChild(f)}
-
-        }
-        
-        
+        let form = document.createElement('table');
+        form.classList = this.formClassList;
+        let tbody = document.createElement('tbody');
+        for(let item in this.data){
+            let row = `<tr><th class="${this.keyClassList}">${this.data[item].name}</th><td class="${this.valueClassList}" contenteditable="true">${this.data[item].value}</td></tr>`
+            tbody.innerHTML += row;            
+        }        
+        form.appendChild(tbody);
         this.container.appendChild(form);
-    }
-    buildFormRow(input){
-        let label = document.createElement('label');
-        label.setAttribute('for', input?.id || '');
-        label.classList = 'col-sm-2 col-form-label text-capitalize';
-        label.innerHTML = input.name;
-        let input_container = document.createElement('div');
-        input_container.classList = 'col-sm-10';
-        input_container.appendChild(input);
-        let row = document.createElement('div');
-        row.classList = 'row mb-1';
-        row.appendChild(label);
-        row.appendChild(input_container);
-        return row;
-    }
+    }    
     
-
 }
 
