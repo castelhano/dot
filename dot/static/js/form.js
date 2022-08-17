75bf005a01a36e8f5458ed46fedc9ddbbcdd201d
Rafael Gustavo Alves <castelhano.rafael@gmail.com>
2022-08-16 21:49:58 -0400

    ajustes jsForm

--- dot/static/js/form.js
+++ dot/static/js/form.js
@@ -22,6 +22,9 @@ class jsForm{
     constructor(options){
         this.data = options?.data || []; // Json com dados do form
         this.container = options?.container || document.body; // parentNode do form, caso nao informado append no document.body
+        this.form = null;
+        this.tbody = null;
+        this.groups = {};
         this.formClassList = options?.formClassList || 'table border';
         this.keyClassList = options?.keyClassList || 'text-capitalize';
         this.valueClassList = options?.valueClassList || 'bg-light border';
@@ -29,15 +32,15 @@ class jsForm{
         this.createForm();
     }
     createForm(){
-        let form = document.createElement('table');
-        form.classList = this.formClassList;
-        let tbody = document.createElement('tbody');
+        this.form = document.createElement('table');
+        this.form.classList = this.formClassList;
+        this.tbody = document.createElement('tbody');
         for(let item in this.data){
             let row = `<tr><th class="${this.keyClassList}">${this.data[item].name}</th><td class="${this.valueClassList}" contenteditable="true">${this.data[item].value}</td></tr>`
-            tbody.innerHTML += row;            
-        }        
-        form.appendChild(tbody);
-        this.container.appendChild(form);
+            this.tbody.innerHTML += row;            
+        }
+        this.form.appendChild(this.tbody);
+        this.container.appendChild(this.form);
     }    
     
 }
