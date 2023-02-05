c29a091dec194020a656342b9e32bacbbe153f5f
Rafael Alves <castelhano.rafael@gmail.com>
2023-02-05 18:11:58 -0400

    Docs para jsSelectm

--- dot/static/js/selectm.js
+++ dot/static/js/selectm.js
@@ -1,9 +1,9 @@
 /*
 * jsSelectm   Implementa controle para select multiple
 *
-* @version  1.00
+* @version  1.06
 * @since    03/02/2023
-* @release  03/02/2023 []
+* @release  05/02/2023 [implementado grupos para o select]
 * @author   Rafael Gustavo Alves {@email castelhano.rafael@gmail.com}
 * @depend   boostrap 5.2.0, fontawesome 5.15.4, dot.css, dot.js
 */
@@ -15,15 +15,15 @@ class jsSelectm{
         this.options = options?.options || this.__initializeOptions();
         this.groups = options?.groups || false; // Informa grupo para os select
         this.title = options?.title || false; // Titulo do select (opcional)
-        this.customStyles = options?.customStyles != undefined ? options.customStyles : false; // Se false criar estilos
+        this.onchange = options?.onchange != undefined ? options.onchange : () => {}; // Funcao a ser chamada ao alterar componente
+        this.disabled = options?.disabled != undefined ? options.disabled : false; // Se sim desativa operacoes nos eventos click e altera formatacao
+        this.reorderOptions = options?.reorderOptions != undefined ? options.reorderOptions : true; // Se sim reordena opcoes baseado no innerText
         // Estilizacao
+        this.customStyles = options?.customStyles != undefined ? options.customStyles : false; // Se false criar estilos
         this.wrapperClassList = options?.wrapperClassList || 'jsSelectm_wrapper'; // Classes o titulo (small) do select 
         this.iconUncheckedClasslist = options?.iconUncheckedClasslist || 'far fa-square fa-fw'; // Classes do icone desmarcado
         this.iconCheckedClasslist = options?.iconCheckedClasslist || 'far fa-check-square fa-fw'; // Classes do icone marcado
-        this.emptySelectMessage = options?.emptySelectMessage || '<p class="text-muted">Nenhuma opção disponivel</p>'; // Mensagem exibida em caso de select vazio
-        this.onchange = options?.onchange != undefined ? options.onchange : () => {}; // Funcao a ser chamada ao alterar componente
-        this.reorderOptions = options?.reorderOptions != undefined ? options.reorderOptions : true; // Se sim reordena opcoes baseado no innerText
-        this.disabled = options?.disabled != undefined ? options.disabled : false; // Se sim desativa operacoes nos eventos click e altera formatacao
+        this.emptySelectMessage = options?.emptySelectMessage || '<p class="text-muted fs-7">Nenhuma opção disponivel</p>'; // Mensagem exibida em caso de select vazio
 
         this.__buildSelect();
         if(!this.customStyles){this.__addStyles();} // Cria estilos padrao caso nao definido estilos customizados
@@ -33,10 +33,8 @@ class jsSelectm{
         let style = document.createElement('style');
         style.innerHTML = '.jsSelectm_wrapper{border: 1px solid #ced4da;border-radius: 0.375rem;padding: 0.375rem 0.875rem 0.475rem 0.75rem;}';
         style.innerHTML += '.jsSelectm_wrapper.disabled{background-color: #E9ECEF;}';
-        style.innerHTML += '.jsSelectm_wrapper:focus-within{border-color: #86b7fe;outline: 0;box-shadow: 0 0 0 0.25rem rgb(13 110 253 / 25%);}';
         style.innerHTML += '.jsSelectm_wrapper small{display: block; margin-bottom: 5px;}';
         style.innerHTML += '.jsSelectm_wrapper > div{max-height:230px;overflow-y: scroll;}';
-        style.innerHTML += '.jsSelectm_wrapper li{background-color: aqua;}';
         style.innerHTML += '.jsSelectm_wrapper div[data-value]{padding: 2px 5px 2px 5px; border-radius: 3px;}';
         style.innerHTML += '.jsSelectm_wrapper div[data-select]{background-color: rgba(25, 135, 84, 0.25)!important;}';
         if(!this.disabled){style.innerHTML += '@media(min-width: 992px){.jsSelectm_wrapper div[data-value]:hover{cursor: pointer;background-color: #ced4da; opacity: 0.5;}}';}
@@ -103,7 +101,7 @@ class jsSelectm{
         if(Object.keys(this.options).length == 0){
             this.optionsContainer.innerHTML = this.emptySelectMessage;
         }
-        else if(this.reorderOptions){this.__reorderOptions();}
+        else if(this.reorderOptions && !this.groups){this.__reorderOptions();}
     }
     __buildGroupsContainer(){
         let acc = document.createElement('div');acc.classList = 'accordion';acc.setAttribute('data-jsSelect-role','group_container');
