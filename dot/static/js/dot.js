8f36dbd851ad773c29789e062d73ac5a2ceeda79
Rafael Alves <castelhano.rafael@gmail.com>
2023-02-22 22:27:36 -0400

    Updated i18n

--- dot/static/js/dot.js
+++ dot/static/js/dot.js
@@ -115,11 +115,11 @@ function dotToday(dias=0, meses=0, anos=0, native=false, el=null){
 * @param    {Element} el Elemento html que ira receber o valor, se omitido retorna a hora na chamada da funcao
 * @returns  {String} Retorna Hora formatada caso nao definido elemento na chamada da funcao
 * @example  
-*    now();           Retorna hora atual (hh:mm)
-*    now(5);          Retorna hora atual somando 5 horas
-*    now(0,0,0, el);  Insere a hora atual no atributo value (ou innerHTML) do elemento informando
+*    dotNow();           Retorna hora atual (hh:mm)
+*    dotNow(5);          Retorna hora atual somando 5 horas
+*    dotNow(0,0,0, el);  Insere a hora atual no atributo value (ou innerHTML) do elemento informando
 */
-function now(horas=0, minutos=0, segundos=false, el=null){
+function dotNow(horas=0, minutos=0, segundos=false, el=null){
   let today = new Date();
   today.setHours(today.getHours() + horas);
   today.setMinutes(today.getMinutes() + minutos);
--- dot/static/js/i18n.js
+++ dot/static/js/i18n.js
@@ -12,18 +12,20 @@ var __langDB = {};
 
 
 function i18n_start(){
+    console.log(`i18n: Start translating at ${dotNow(0,0,true)}`);
     document.querySelectorAll('[data-i18n]').forEach((e) => {
         try{
-            e.innerHTML = e.dataset.i18n.split('.').reduce((previous, current) => previous[current], __langDB) || e.innerHTML;
+            let result = e.dataset.i18n.split('.').reduce((previous, current) => previous[current], __langDB);
+            e.innerHTML =  result || e.innerHTML;
+            if(!result){trow}
         }
-        catch(error){
-            console.log(`i18n: Entry [${e.dataset.i18n}] not found for ${__lang}`);
-        }
+        catch(error){console.log(`i18n: Entry [${e.dataset.i18n}] not found for (${__lang})`)}
     })
-
+    console.log(`i18n: End translating at ${dotNow(0,0,true)}`);
 }
 
 if(__lang != __langDefault){ // Se language do cliente for diferente da default, carrega base com traducoes
+    console.log(`i18n: Sending request to server at ${dotNow(0,0,true)}`);
     dotAppData(`/app_data/i18n__${__lang}.json`).then((d) => {
         if(d != ''){__langDB = d}
         i18n_start();        
