71c13b53e5069424d555f0477664269dbd9e8c95
castelhano <castelhano.rafael@gmail.com>
2023-03-08 16:02:08 -0400

    jsSelectm add markAll

--- dot/static/js/selectm.js
+++ dot/static/js/selectm.js
@@ -1,9 +1,9 @@
 /*
 * jsSelectm   Implementa controle para select multiple
 *
-* @version  1.06
+* @version  1.08
 * @since    03/02/2023
-* @release  05/02/2023 [implementado grupos para o select]
+* @release  08/03/2023 [adicionado marcar todos]
 * @author   Rafael Gustavo Alves {@email castelhano.rafael@gmail.com}
 * @depend   boostrap 5.2.0, fontawesome 5.15.4, dot.css, dot.js
 */
@@ -17,6 +17,7 @@ class jsSelectm{
         this.title = options?.title || false; // Titulo do select (opcional)
         this.onchange = options?.onchange != undefined ? options.onchange : () => {}; // Funcao a ser chamada ao alterar componente
         this.disabled = options?.disabled != undefined ? options.disabled : false; // Se sim desativa operacoes nos eventos click e altera formatacao
+        this.checkAll = options?.checkAll != undefined ? options.checkAll : true; // Se sim sera adicionado botao para marcar todas as opcoes
         this.reorderOptions = options?.reorderOptions != undefined ? options.reorderOptions : true; // Se sim reordena opcoes baseado no innerText
         // Estilizacao
         this.customStyles = options?.customStyles != undefined ? options.customStyles : false; // Se false criar estilos
@@ -31,14 +32,14 @@ class jsSelectm{
     }
     __addStyles(){
         let style = document.createElement('style');
-        style.innerHTML = '.jsSelectm_wrapper{border: 1px solid #ced4da;border-radius: 0.375rem;padding: 0.375rem 0.875rem 0.475rem 0.75rem;}';
+        style.innerHTML = '.jsSelectm_wrapper{position:relative;border: 1px solid #ced4da;border-radius: 0.375rem;padding: 0.375rem 0.875rem 0.475rem 0.75rem;}';
         style.innerHTML += '.jsSelectm_wrapper.disabled{background-color: #E9ECEF;}';
         style.innerHTML += '[data-bs-theme="dark"] .jsSelectm_wrapper.disabled{background-color: #393939;}';
-        style.innerHTML += '.jsSelectm_wrapper small{display: block; margin-bottom: 5px;}';
+        style.innerHTML += '.jsSelectm_wrapper small{margin-bottom: 5px;margin-right: 5px;}';
         style.innerHTML += '.jsSelectm_wrapper > div{max-height:230px;overflow-y: scroll;}';
-        style.innerHTML += '.jsSelectm_wrapper div[data-value]{padding: 2px 5px 2px 5px; border-radius: 3px;}';
+        style.innerHTML += '.jsSelectm_wrapper div[data-value], .jsSelectm_wrapper div[data-role]{padding: 2px 5px 2px 5px; border-radius: 3px;}';
         style.innerHTML += '.jsSelectm_wrapper div[data-select]{background-color: rgba(25, 135, 84, 0.25)!important;}';
-        if(!this.disabled){style.innerHTML += '@media(min-width: 992px){.jsSelectm_wrapper div[data-value]:hover{cursor: pointer;background-color: #ced4da; opacity: 0.5;}}';}
+        if(!this.disabled){style.innerHTML += '@media(min-width: 992px){.jsSelectm_wrapper div[data-value]:hover, .jsSelectm_wrapper div[data-role]:hover{cursor: pointer;background-color: #ced4da; opacity: 0.5;}}';}
         document.getElementsByTagName('head')[0].appendChild(style);
     }
     __buildSelect(){
@@ -49,6 +50,8 @@ class jsSelectm{
             this.titleEl = document.createElement('small');this.titleEl.innerHTML = this.title;
             this.wrapper.appendChild(this.titleEl);
         }
+        this.loadingStatus = document.createElement('span');this.loadingStatus.style = 'position:absolute;top:5px;right:5px;display:none;';this.loadingStatus.innerHTML = '<div class="spinner-grow text-success"></div>';
+        this.wrapper.appendChild(this.loadingStatus);
         this.optionsContainer = document.createElement('div');this.optionsContainer.style.marginTop = '5px';
         this.wrapper.appendChild(this.optionsContainer);
         this.target.after(this.wrapper);
@@ -66,11 +69,10 @@ class jsSelectm{
     __reorderOptions(){
         let items = [...this.optionsContainer.querySelectorAll('[data-value]')];
         let reordered = items.sort(function(a, b) {
-            return a.innerText == b.innerText
-            ? 0
-            : (a.innerText > b.innerText ? 1 : -1);
+            return a.innerText == b.innerText ? 0 : (a.innerText > b.innerText ? 1 : -1);
         });
         this.optionsContainer.innerHTML = '';
+        if(this.checkAll){this.__addCheckAll(this.optionsContainer);}
         for(let i in reordered){
             this.optionsContainer.appendChild(reordered[i]);
         }
@@ -112,14 +114,48 @@ class jsSelectm{
             let acc_button = document.createElement('span');acc_button.classList = 'accordion-button collapsed fs-6 py-2';acc_button.setAttribute('data-bs-toggle','collapse');acc_button.setAttribute('data-bs-target',`[data-group=${i}]`);
             acc_button.innerHTML = i;
             let acc_container = document.createElement('div');acc_container.classList = 'accordion-collapse collapse';acc_container.setAttribute('data-group', i);acc_container.setAttribute('data-bs-parent', '[data-jsSelect-role=group_container]');
-            let acc_body = document.createElement('div');acc_body.classList = 'accordion-collapse collapse';
-            acc_container.appendChild(acc_body);
+            if(this.checkAll){this.__addCheckAll(acc_container);}
             acc_header.appendChild(acc_button);
             acc_item.appendChild(acc_header);
             acc_item.appendChild(acc_container);
             acc.appendChild(acc_item);
             this.optionsContainer.appendChild(acc);
         }
+    }
+    __addCheckAll(container){
+        let selectAll = document.createElement('div');selectAll.setAttribute('data-role', 'checkAll');
+        let checkIcon = document.createElement('i');checkIcon.classList = this.iconUncheckedClasslist;
+        let optionTxt = document.createElement('span');optionTxt.classList = 'fs-7 text-secondary';optionTxt.innerHTML = 'Marcar todos';
+        selectAll.onclick = (e) => {
+            this.loadingStatus.style.display = 'inline-block';
+            this.target.innerHTML = ''; // Limpa as options do target pois nao sera chamado funcao rebuildTargetOptions por questoes de performance
+            setTimeout(() => { // Adiciona timeout para exibir this.loadingStatus antes de inicar processo..
+                if(selectAll.dataset.checked != undefined){
+                    checkIcon.classList = this.iconUncheckedClasslist;
+                    selectAll.removeAttribute('data-checked');
+                    this.optionsSelected = [];
+                    selectAll.parentNode.querySelectorAll('[data-select]').forEach((el) => {
+                        el.removeAttribute('data-select');
+                        el.querySelector('i').classList = this.iconUncheckedClasslist;
+                    });
+                }
+                else{
+                    checkIcon.classList = this.iconCheckedClasslist;
+                    selectAll.setAttribute('data-checked', '');
+                    this.optionsSelected = Object.keys(this.options);
+                    selectAll.parentNode.querySelectorAll('div[data-value]:not([data-select])').forEach((el) => {
+                        el.setAttribute('data-select', '');
+                        el.querySelector('i').classList = this.iconCheckedClasslist;
+                        this.target.innerHTML += `<option value="${el.dataset.value}" selected>${el.innerText}</option>`; // Insere option no target select para cada opcao selecionada
+                    });
+                }
+                this.loadingStatus.style.display = 'none';
+                this.onchange();
+            }, 15);
+        };
+        selectAll.appendChild(checkIcon);
+        selectAll.appendChild(optionTxt);
+        container.appendChild(selectAll);
     }
     __getGroupContainer(key){ // Localiza se item esta em algum grupo, se sim retorna o container do grupo, caso nao retorna o container principal
         for(let group in this.groups){
@@ -141,10 +177,10 @@ class jsSelectm{
         this.rebuildTargetOptions();
     }
     rebuildTargetOptions(){
-        this.target.innerHTML = ''; // Limpa os options
-        this.optionsContainer.querySelectorAll('[data-select]').forEach((e) => {
+        this.optionsContainer.querySelectorAll('[data-select]').forEach((e, i) => {
             this.target.innerHTML += `<option value="${e.dataset.value}" selected>${e.innerText}</option>`;
         })
+        this.loadingStatus.style.display = 'none'; // Oculta loadingStatus (caso exibido)
         this.onchange();
     }
 }
