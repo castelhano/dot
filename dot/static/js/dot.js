3800118d42224fe3dfc4ede76f920abb9fca69a5
castelhano <castelhano.rafael@gmail.com>
2023-03-14 16:16:06 -0400

    Ajuste dotNotify

--- dot/static/js/dot.js
+++ dot/static/js/dot.js
@@ -16,7 +16,7 @@ function dotAlert(tipo, mensagem, autodismiss=true){
   try {document.querySelector('[data-type="dotAlert"]').remove();}catch(e){}let e = document.createElement('div');e.setAttribute('data-type','dotAlert');e.style.zIndex = 100;let b = document.createElement('button');b.classList.add('btn-close');b.setAttribute('data-bs-dismiss','alert');e.classList.add('alert','slideIn','dotAlert',`alert-${tipo}`,'alert-dismissible','fade','show','mb-0');e.innerHTML = mensagem;e.appendChild(b);document.body.appendChild(e);if(autodismiss){setTimeout(function() {e.remove()}, 5000);}}
 
 function dotNotify(tipo, mensagem, autodismiss=true){
-  try {document.querySelector('[data-type="dotAlert"]').remove();}catch(e){} let e = document.createElement('div'); e.classList = `alert alert-${tipo} alert-dismissible slideIn mb-2`; let b = document.createElement('button'); b.classList = 'btn-close'; b.setAttribute('data-bs-dismiss','alert'); e.innerHTML = mensagem; e.appendChild(b); document.getElementById('notify_container').appendChild(e); if(autodismiss){setTimeout(function() {e.remove()}, 4500);} }
+  let e = document.createElement('div'); e.classList = `alert alert-${tipo} alert-dismissible slideIn mb-2`; let b = document.createElement('button'); b.classList = 'btn-close'; b.setAttribute('data-bs-dismiss','alert'); e.innerHTML = mensagem; e.appendChild(b); document.getElementById('notify_container').appendChild(e); if(autodismiss){setTimeout(function() {e.remove()}, 4500);} }
 
 /*
 * getCookie Busca no arquivo de cookie pela chave informada e retorna o valor
